b486657fd52a695061192324f09e1e0fb107fa7c
Xingrui Song <songx@wustl.edu>
2019-11-18 17:15:00 -0600

    Modified the g-code files using python script

--- python/modify_drills.py
+++ python/modify_drills.py
@@ -2,12 +2,15 @@ from gcode_tools import *
 
 gcode_dir = r"../g-code/"
 
-drill_tool(gcode_dir + "ttc.txt", Z=-0.015, pecking=True)
-drill_tool(gcode_dir + "tt43.txt", Z=-0.620, pecking=True)
-drill_tool(gcode_dir + "tt32.txt", Z=-0.620, pecking=True)
-drill_tool(gcode_dir + "tt3mm.txt", Z=-0.620, pecking=True)
-drill_tool(gcode_dir + "btc.txt", Z=-0.015, pecking=True)
-drill_tool(gcode_dir + "bt45.txt", Z=-0.0667, pecking=True)
-drill_tool(gcode_dir + "bt43.txt", Z=-0.300, pecking=True)
-drill_tool(gcode_dir + "bt3mm.txt", Z=-0.300, pecking=True)
+drill_tool(gcode_dir + "xtc43.txt", Z=-0.015, pecking=True)
+drill_tool(gcode_dir + "xtc32.txt", Z=-0.135, pecking=True)
+drill_tool(gcode_dir + "xtc3mm.txt", Z=-0.135, pecking=True)
+drill_tool(gcode_dir + "xt43.txt", Z=-0.620, pecking=True)
+drill_tool(gcode_dir + "xt32.txt", Z=-0.620, pecking=True)
+drill_tool(gcode_dir + "xt3mm.txt", Z=-0.620, pecking=True)
+drill_tool(gcode_dir + "ytc45.txt", Z=-0.015, pecking=True)
+drill_tool(gcode_dir + "ytc3mm.txt", Z=-0.015, pecking=True)
+drill_tool(gcode_dir + "yt45.txt", Z=-0.0667, pecking=True)
+drill_tool(gcode_dir + "yt43.txt", Z=-0.300, pecking=True)
+drill_tool(gcode_dir + "yt3mm.txt", Z=-0.300, pecking=True)
 
